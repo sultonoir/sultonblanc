a5c357368ea9943f74957b7c234cf01edeac22f1
Sulton <sultonalidrus5@gmail.com>
2024-02-18 22:15:59 +0700

    ref : add id experience

--- src/template/home/Experience.tsx
+++ src/template/home/Experience.tsx
@@ -10,6 +10,7 @@ const Experience = () => {
   return (
     <div
       ref={ref}
+      id="Experience"
       className="mb-28 size-full">
       <SectionHeading>My Experience</SectionHeading>
       <div className="container grid max-w-screen-sm grid-cols-1 gap-5 ">
